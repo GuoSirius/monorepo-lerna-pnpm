52770ad6149f5620426e024fbf3472f071a383ab
郭之存 <guoplc@163.com>
2021-03-20 13:13:44 +0800

    PK

--- packages/water-pipe/src/views/canvas-dashboard/CanvasDashboard.vue
+++ packages/water-pipe/src/views/canvas-dashboard/CanvasDashboard.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="wrapper">
     <canvas ref="canvas" class="canvas"></canvas>
+
+    <div v-if="!NEED_DRAW_TEXT" class="text">
+      <span class="text-title">最高额度</span>
+      <span class="text-value">{{ canvasDashborad ? canvasDashborad.currentValue : 0 }}</span>
+    </div>
   </div>
 </template>
 
@@ -9,9 +14,18 @@ import CanvasDashboard from './CanvasDashboard.js'
 
 import bocIcon from './images/boc.png'
 
+const NEED_DRAW_TEXT = false
+
 export default {
   name: 'CanvasDashboard',
+  data() {
+    return {
+      canvasDashborad: null
+    }
+  },
   created() {
+    this.NEED_DRAW_TEXT = NEED_DRAW_TEXT
+
     this.canvasDashborad = null
 
     // this.mockRequest()
@@ -34,7 +48,7 @@ export default {
         this.initialize()
 
         this.canvasDashborad.setValue(0, 10000, true)
-        // this.canvasDashborad.setValue(10000, 10000000, true)
+        this.canvasDashborad.setValue(10000, 10000000, true)
       })
     },
     initialize() {
@@ -43,7 +57,12 @@ export default {
 
       if (!canvas || canvasDashborad) return
 
-      const _canvasDashborad = new CanvasDashboard(canvas, { radius: 240, icon: bocIcon, titleOffsetY: 10 })
+      const _canvasDashborad = new CanvasDashboard(canvas, {
+        icon: bocIcon,
+        radius: 240,
+        titleOffsetY: 10,
+        needDrawText: NEED_DRAW_TEXT
+      })
 
       this.canvasDashborad = _canvasDashborad
       window.canvasDashborad = _canvasDashborad
@@ -63,11 +82,34 @@ export default {
 
 <style lang="scss" scoped>
 .wrapper {
+  position: relative;
   text-align: center;
 
   .canvas {
     margin-top: 48px;
     background-color: pink;
+  }
+
+  .text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 24px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: #fff;
+    font-weight: bold;
+
+    &-title {
+      margin-bottom: 36px;
+      font-size: 18px;
+    }
+
+    &-value {
+      font-size: 36px;
+    }
   }
 }
 </style>
